d214bfd929a2f1b7ca11c10539cc881e731eb80e
mslaursen <martin_laursen21@hotmail.com>
2025-01-12 22:44:17 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="fastapi-forge",
-    version="0.1.0",
+    version="0.1.1",
     packages=find_packages(),
     package_data={"fastapi_forge": ["static/*", "templates/*"]},
     install_requires=[
