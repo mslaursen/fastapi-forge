03bfeddafbfbb6b3c1acd694172bf81a9d692302
mslaursen <martin_laursen21@hotmail.com>
2025-01-13 01:05:36 +0100

    Create app with health route using cookiecutter

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="fastapi-forge",
-    version="0.1.2",
+    version="0.1.3",
     packages=find_packages(),
     package_data={"fastapi_forge": ["static/*", "templates/*"]},
     install_requires=[
