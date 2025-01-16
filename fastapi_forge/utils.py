ad522a21e92b5d84650f3b71f19b39f702d69bb9
mslaursen <martin_laursen21@hotmail.com>
2025-01-17 00:51:47 +0100

    Generate DTOs and validate project request DTO

--- fastapi_forge/utils.py
+++ fastapi_forge/utils.py
@@ -1,6 +1,24 @@
 import webbrowser
+from .dtos import Model
+from .jinja import render_models_to_dtos, render_models_to_models
+import os
 
 
 def open_browser(url: str) -> None:
     """Opens a web browser to the specified URL."""
     webbrowser.open(url)
+
+
+def generate_extra(project_name: str, models: list[Model]) -> None:
+    """Generates extra files for the project."""
+
+    file_to_func = {
+        "models.py": render_models_to_models,
+        "dtos.py": render_models_to_dtos,
+    }
+
+    for file, func in file_to_func.items():
+        file = os.path.join(os.getcwd(), project_name, "src", file)
+
+        with open(file, "w") as file:
+            file.write(func(models))
