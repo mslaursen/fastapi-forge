1919d5e62cb25b2622da90a1af5a7a998da3fd20
mslaursen <martin_laursen21@hotmail.com>
2025-01-26 14:12:18 +0100

    v0.2.0

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="fastapi-forge",
-    version="0.1.14",
+    version="0.2.0",
     packages=find_packages(),
     install_requires=[
         "fastapi",
