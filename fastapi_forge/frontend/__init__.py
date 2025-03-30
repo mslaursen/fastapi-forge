55c755b8e54d60cd9f8c39aa595a86ff954cbcae
mslaursen <martin_laursen21@hotmail.com>
2025-03-30 12:25:05 +0200

    Clean up state.py

--- fastapi_forge/frontend/__init__.py
+++ fastapi_forge/frontend/__init__.py
@@ -4,3 +4,4 @@ from fastapi_forge.frontend.components.model_row import ModelRow
 from fastapi_forge.frontend.panels.model_panel import ModelPanel
 from fastapi_forge.frontend.panels.model_editor_panel import ModelEditorPanel
 from fastapi_forge.frontend.panels.project_config_panel import ProjectConfigPanel
+from fastapi_forge.frontend.main import init
