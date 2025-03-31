a8a0fa9ad42da436e6507b6397272898cf2bf40a
mslaursen <martin_laursen21@hotmail.com>
2025-03-31 21:56:44 +0200

    Fin

--- tests/test_dtos.py
+++ tests/test_dtos.py
@@ -60,7 +60,7 @@ def test_invalid_field_name(invalid_name: str) -> None:
         (FieldDataType.INTEGER, 'factory.Faker("random_int")'),
         (
             FieldDataType.FLOAT,
-            'factory.Faker("pyfloat", posistive=True, min_value=0.1, max_value=100)',
+            'factory.Faker("pyfloat", positive=True, min_value=0.1, max_value=100)',
         ),
         (FieldDataType.BOOLEAN, 'factory.Faker("boolean")'),
         (FieldDataType.DATETIME, 'factory.Faker("date_time")'),
