a22d8a7edbec2f3c974d1e4fb87912bac0204d6f
mslaursen <82436992+mslaursen@users.noreply.github.com>
2025-04-19 18:18:34 +0200

    update: item row name truncated (#36)

--- fastapi_forge/frontend/constants.py
+++ fastapi_forge/frontend/constants.py
@@ -5,6 +5,7 @@ from fastapi_forge.enums import FieldDataTypeEnum
 
 SELECTED_MODEL_TEXT_COLOR = "text-black-500 dark:text-amber-300"
 SELECTED_ENUM_TEXT_COLOR = "text-black-500 dark:text-amber-300"
+ITEM_ROW_TRUNCATE_LEN = 17
 
 FIELD_COLUMNS: list[dict[str, Any]] = [
     {
