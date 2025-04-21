b216c6324d638d8bbf46374bec18a51654ac24ce
mslaursen <82436992+mslaursen@users.noreply.github.com>
2025-04-21 12:59:40 +0200

    update: improved model name pluralization (#45)

--- fastapi_forge/frontend/validation.py
+++ fastapi_forge/frontend/validation.py
@@ -1,5 +1,3 @@
-
-
 def raise_if_missing_fields(required: list[tuple[str, str | None]]):
     missing_required = [field_name for field_name, kwarg in required if not kwarg]
     if missing_required:
