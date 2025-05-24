48eaca0d38a5503210539725fc374ecf57a59a0a
mslaursen <82436992+mslaursen@users.noreply.github.com>
2025-05-24 12:27:03 +0200

    feat(Option): Logfire 🔥 (#89)

--- fastapi_forge/template/{{cookiecutter.project_name}}/{{cookiecutter.project_name}}/middleware.py
+++ fastapi_forge/template/{{cookiecutter.project_name}}/{{cookiecutter.project_name}}/middleware.py
@@ -4,6 +4,10 @@ from {{cookiecutter.project_name}}.settings import settings
 {% if cookiecutter.use_prometheus %}
 from prometheus_fastapi_instrumentator import Instrumentator
 {% endif %}
+{% if cookiecutter.use_logfire %}
+import logfire
+from loguru import logger
+{% endif %}
 
 def _add_cors_middleware(app: FastAPI) -> None:
     """Add CORS Middleware."""
@@ -11,13 +15,39 @@ def _add_cors_middleware(app: FastAPI) -> None:
 {% if cookiecutter.use_prometheus %}
 def _add_prometheus_middleware(app: FastAPI) -> None:
     """Add Prometheus Middleware."""
-    if settings.prometheus.enabled:
-        instrumenter = Instrumentator().instrument(app)
-        instrumenter.expose(app)
+    if not settings.prometheus.enabled:
+        return
+    instrumenter = Instrumentator().instrument(app)
+    instrumenter.expose(app)
+{% endif %}
+{% if cookiecutter.use_logfire %}
+def _add_logfire_middleware(app: FastAPI) -> None:
+    """Add Logfire Middleware."""
+    if not settings.logfire.enabled:
+        return
+    if not settings.logfire.write_token:
+        logger.warning(
+            "Logfire is enabled but no write token is provided. "
+            "Skipping Logfire middleware."
+        )
+        return
+    logfire.configure(
+        token=settings.logfire.write_token.get_secret_value(),
+        environment=settings.env,
+        send_to_logfire="if-token-present",
+        service_name="game_zone",
+    )
+    logfire.instrument_fastapi(app, capture_headers=True)
+    logfire.instrument_asyncpg()
+    logfire.instrument_system_metrics()
+    logger.configure(handlers=[logfire.loguru_handler()])
 {% endif %}
 def add_middleware(app: FastAPI) -> None:
     """Add all middlewares."""
     _add_cors_middleware(app)
     {%- if cookiecutter.use_prometheus %}
     _add_prometheus_middleware(app)
+    {% endif %}
+    {%- if cookiecutter.use_logfire %}
+    _add_logfire_middleware(app)
     {% endif %}
