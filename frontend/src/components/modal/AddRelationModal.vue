a63fa159814f362e0996cf2c35d25049edcb75a5
mslaursen <82436992+mslaursen@users.noreply.github.com>
2025-08-05 00:21:27 +0200

    update(Frontend v2): Modal stylings (#101)

--- frontend/src/components/modal/AddRelationModal.vue
+++ frontend/src/components/modal/AddRelationModal.vue
@@ -1,13 +1,13 @@
 <template>
   <main class="relation-container">
-    <div class="grid-2x2">
+    <div class="input-container">
       <div class="input-group">
-        <label class="relation-label">Field Name:</label>
+        <label class="relation-label">Field name</label>
         <input class="relation-input" v-model="fieldName" type="text" />
       </div>
 
       <div class="input-group">
-        <label class="relation-label">Target Model:</label>
+        <label class="relation-label">Target model</label>
         <select class="relation-select" v-model="selectedNodeId">
           <option disabled value="">-- Select a model --</option>
           <option v-for="node in filteredNodes" :key="node.id" :value="node.id">
@@ -17,7 +17,7 @@
       </div>
 
       <div class="input-group">
-        <label class="relation-label">OnDelete:</label>
+        <label class="relation-label">OnDelete</label>
         <select class="relation-select" v-model="onDelete">
           <option disabled value="">-- Select behavior --</option>
           <option value="CASCADE">CASCADE</option>
@@ -26,7 +26,7 @@
       </div>
 
       <div class="input-group">
-        <label class="relation-label">Back Populates:</label>
+        <label class="relation-label">Back populates</label>
         <input class="relation-input" v-model="backPopulates" type="text" />
       </div>
     </div>
@@ -37,7 +37,9 @@
       <label><input type="checkbox" v-model="indexed" /> Index</label>
     </div>
 
-    <button class="save-relation-btn" @click="saveSelect">Save</button>
+    <div class="action-group">
+      <button class="save-relation-btn" @click="saveSelect">Save</button>
+    </div>
   </main>
 </template>
 
@@ -65,7 +67,6 @@ const filteredNodes = computed(() => projectStore.nodes.filter((node) => node.id
 
 const saveSelect = () => {
   if (!selectedNodeId.value || !fieldName.value) return
-
   projectStore.addRelation(props.id, selectedNodeId.value, {
     fieldName: fieldName.value,
     targetModel: selectedNodeId.value,
@@ -75,7 +76,6 @@ const saveSelect = () => {
     isUnique: unique.value,
     isIndex: indexed.value,
   })
-
   modalStore.close()
 }
 </script>
@@ -85,60 +85,53 @@ const saveSelect = () => {
   display: flex;
   flex-direction: column;
   gap: 1rem;
-  border: 2px solid black;
   padding: 1rem;
-  box-shadow: 4px 4px 0px black;
-  background-color: #fff;
 }
 
-.grid-2x2 {
-  display: grid;
-  grid-template-columns: 1fr 1fr;
+.input-container {
+  display: flex;
+  flex-direction: column;
   gap: 1rem;
 }
 
 .input-group {
   display: flex;
   flex-direction: column;
+  gap: 0.15rem;
 }
 
 .relation-label {
   font-weight: bold;
-  margin-bottom: 0.25rem;
+  margin-bottom: 5px;
 }
 
 .relation-input,
 .relation-select {
   border: 2px solid black;
-  border-radius: 4px;
-  padding: 0.5rem;
-  box-shadow: 3px 3px 0px black;
-  background-color: var(--color-background);
-  transition:
-    transform 0.1s ease-in-out,
-    box-shadow 0.1s ease-in-out;
-}
-
-.relation-input:focus,
-.relation-select:focus {
-  outline: none;
-  transform: translate(2px, 2px);
-  box-shadow: none;
+  border-radius: 6px;
+  padding: 0.6rem;
+  background-color: white;
 }
 
 .checkbox-group {
   display: flex;
-  justify-content: space-between;
-  gap: 1rem;
+  flex-direction: column;
+  gap: 0.5rem;
   font-weight: bold;
 }
 
+.action-group {
+  gap: 0.5rem;
+  margin-top: 2rem;
+  display: flex;
+  flex-direction: column;
+}
+
 .save-relation-btn {
-  align-self: flex-end;
+  width: 100%;
   padding: 0.5rem 1rem;
   border: 2px solid black;
   border-radius: 4px;
-  font-weight: bold;
   background-color: var(--color-success);
   box-shadow: 3px 3px 0px black;
   transition:
